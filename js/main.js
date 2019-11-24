c872852ce45ca7671851f06eb514f4d0445c984c
armenkhachatryan <43514007+armenkhachatryan@users.noreply.github.com>
2019-11-24 20:40:37 +0400

    modified js file

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 // let bill = [124,48,268];
 let user = {
     name: 'Armen',
-    bill:[124,48,268,180,42],
+    bill:[124,268,180,402],
     tip: [],
-    rezult:[],
+    rezultArmen:[],
     meth: function calculator (){
             let percantage;
             for(let i = 0; i<this.bill.length;i++){
                 if(this.bill[i]<50){
                     percantage = this.bill[i] *0.2;
                     this.tip.push(Math.round(percantage));
-                    this.rezult.push(this.bill[i]+percantage)
+                    this.rezultArmen.push(this.bill[i]+percantage)
                 }
                 else if(this.bill[i]>50 && this.bill[i]<200){
-                    percantage = this.bill[i] *0.2;
+                    percantage = this.bill[i] *0.1;
                     this.tip.push(Math.round(percantage));
-                    this.rezult.push(this.bill[i]+percantage);
+                    this.rezultArmen.push(this.bill[i]+percantage);
                 }
                 else {
-                    percantage = this.bill[i] *0.2;
+                    percantage = this.bill[i] *0.25;
                     this.tip.push(Math.round(percantage));
-                    this.rezult.push(this.bill[i]+percantage  );
+                    this.rezultArmen.push(this.bill[i]+percantage  );
                 }
     
             }
-console.log(this.tip);
-console.log(this.rezult);
+// console.log(this.tip);
+console.log(this.rezultArmen);
         }
     }
 user.meth();
@@ -57,35 +57,53 @@ user.meth();
         console.log(jane.bmi)*/
 
         let user1 = {
-            name: 'Gugush',
-            bill:[77,375,119,45],
+            name: 'Gayush',
+            bill:[124,268,180,402],
             tip: [],
-            rezult:[],
+            rezultGayush:[],
             meth: function calculator (){
                     let percantage;
                     for(let i = 0; i<this.bill.length;i++){
                         if(this.bill[i]<100){
                             percantage = this.bill[i] *0.2;
                             this.tip.push(Math.round(percantage));
-                            this.rezult.push(this.bill[i]+percantage)
+                            this.rezultGayush.push(this.bill[i]+percantage)
                         }
                         else if(this.bill[i]>=100 && this.bill[i]<300){
                             percantage = this.bill[i] *0.1;
                             this.tip.push(Math.round(percantage));
-                            this.rezult.push(this.bill[i]+percantage);
+                            this.rezultGayush.push(this.bill[i]+percantage);
                         }
                         else {
                             percantage = this.bill[i] *0.25;
                             this.tip.push(Math.round(percantage));
-                            this.rezult.push(this.bill[i]+percantage  );
+                            this.rezultGayush.push(this.bill[i]+percantage  );
                         }
             
                     }
-        console.log(this.tip);
-        console.log(this.rezult);
+        // console.log(this.tip);
+        console.log(this.rezultGayush);
                 }
             }
         user1.meth();
+        function average(arr){
+            let sum =0 ;
+            for (let i=0; i<arr.length; i++ ){
+             
+                sum += arr[i] ;
+            };
+            let aver = sum/arr.length;
+            console.log(sum);
+            return(aver)
+        }
+        if (average(user.rezultArmen)>average(user1.rezultGayush)){
+            console.log(`${user.name}s family spend more then ${user1.name}s!`)
+        } else if (average(user.rezultArmen)=== average(user1.rezultGayush)){
+            console.log(`${user1.name}s family spend equale with ${user.name}s!`)
+        }else {
+            console.log(`${user1.name}s family spend moren then ${user.name}s!`)
+        }
+        
 
 
 
